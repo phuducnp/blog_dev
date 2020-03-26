19aed7a2e746ba5562446160b1b6d02163c79b7f
ducnpp <ducnpp@runsystem.net>
2020-03-26 15:25:41 +0700

    fix prd key

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.secret_key_base = 'ngophamphuduc'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
