eb0affc821204391a036f77b21c973476378469d
ducnpp <ducnpp@runsystem.net>
2020-03-30 14:56:10 +0700

    update csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   rescue_from ActiveRecord::RecordNotFound, :with => :error404
 end
